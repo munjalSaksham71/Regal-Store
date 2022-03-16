df26faf52bd8bef3129be08abc795846f6ea54a0
munjalSaksham71 <sakshammunjal4@gmail.com>
2022-03-16 13:34:51 +0530

    Using Async-await

--- src/context/product-context.js
+++ src/context/product-context.js
@@ -6,35 +6,38 @@ const ProductContext = createContext();
 const useProduct = () => useContext(ProductContext);
 
 const initialState = {
-    loading: true, 
-    products: [],
-    error: ''
-}
+  loading: true,
+  products: [],
+  error: "",
+};
 
 const getProducts = (state, action) => {
-    switch (action.type) {
-        case 'SUCCESS':
-            return {...state, products: action.payload, loading: false};
-        case 'ERROR':
-            return {...state, loading: false, error: "Something went wrong"};
-        default:
-           return state;
+  switch (action.type) {
+    case "SUCCESS":
+      return { ...state, products: action.payload, loading: false };
+    case "ERROR":
+      return { ...state, loading: false, error: "Something went wrong" };
+    default:
+      return state;
+  }
+};
+
+const ProductProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(getProducts, initialState);
+
+  useEffect(async () => {
+    try {
+      const response = await axios.get("/api/products");
+      dispatch({ type: "SUCCESS", payload: response.data });
+    } catch (error) {
+      dispatch({ type: "ERROR" });
     }
-}
+  }, []);
 
-const ProductProvider = ({children}) => {
-    useEffect(() => {  
-        axios.get('/api/products')  
-            .then(response => {  
-                dispatch({ type: 'SUCCESS', payload: response.data })  
-            })  
-            .catch(error => {  
-                dispatch({ type: 'ERROR'})  
-            })  
-    }, [])
-
-    const [state, dispatch] = useReducer(getProducts, initialState)
-
-    return <ProductContext.Provider value={{state, dispatch}}>{children}</ProductContext.Provider>
-}
-export {useProduct, ProductProvider}
+  return (
+    <ProductContext.Provider value={{ state, dispatch }}>
+      {children}
+    </ProductContext.Provider>
+  );
+};
+export { useProduct, ProductProvider };
