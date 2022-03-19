160242c52a550d8a54e85fbafcc23f9d67ae1982
munjalSaksham71 <sakshammunjal4@gmail.com>
2022-03-19 13:05:23 +0530

    Refactor code

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,21 +1,20 @@
 import { Link } from 'react-router-dom';
 import { useFilters } from '../../context/filter-context';
 import './Categories.css'
-const Categories = (props) => {
-    const {category} = props;
+const Categories = ({categories}) => {
     const { productListDispatch } = useFilters()
     return (
         <div className="category">   
-            {category.map((c) => (
-                <div key={c._id} className="card m-2 up-curve-border">
-                <img className="up-curve-border" src={c.image} />
+            {categories.map((category) => (
+                <div key={category._id} className="card m-2 up-curve-border">
+                <img className="up-curve-border" src={category.image} />
                 <div className="card_main pl-3 mt-2 mb-1">
-                    <div className="card_topic mb-1">{c.categoryName}</div>
+                    <div className="card_topic mb-1">{category.categoryName}</div>
                 </div>
-                <div className="card_content pl-3 pr-1 mt-2">{c.description}</div>
+                <div className="card_content pl-3 pr-1 mt-2">{category.description}</div>
                 <div className="card_footer mt-2 mb-2 pl-3">
                     <div className="card_buttons">
-                        <Link to='/products'><button className="btn btn-primary" onClick={() => productListDispatch({type: 'FILTER_BY_CATEGORY', payload: c.categoryName})}>See Products</button> </Link>
+                        <Link to='/products'><button className="btn btn-primary" onClick={() => productListDispatch({type: 'FILTER_BY_CATEGORY', payload: category.categoryName})}>See Products</button> </Link>
                     </div>
                 </div>
                 </div>
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -4,14 +4,17 @@ import Categories from '../components/Categories/Categories';
 import Navbar from '../components/Nav/Navbar';
 
 const HomeScreen = () => {
-    const [category, setCategory] = useState([]);
+    const [categories, setCategories] = useState([]);
     const [error, setError] = useState("");
+    const [loading, setLoading] = useState(true);
     useEffect(() => {
         (async() => {
             try {
                 const { data: { categories } } = await axios.get('/api/categories');
-                setCategory(categories);
+                setLoading(false);
+                setCategories(categories);
             } catch (error) {
+                setLoading(false);
                 setError("Something Went Wrong");
             }
         })()    
@@ -20,8 +23,9 @@ const HomeScreen = () => {
     return (
         <div>
             <Navbar />
+            {loading && <div>Loading....</div>}
             <p className="heading1 center">Product Categories</p>
-            {!error && <Categories category={category} />}
+            {!error && !loading && <Categories categories={categories} />}
             {error && <div>{error}</div>}
         </div>
     )
