36486ac9ee8889d96cdfe3486fac9663e7e5aa42
munjalSaksham71 <sakshammunjal4@gmail.com>
2022-04-07 09:00:19 +0530

    Format Document

--- src/screens/NotFound.jsx
+++ src/screens/NotFound.jsx
@@ -3,16 +3,16 @@ import "../App.css";
 
 const NotFound = () => {
   return (
-      <div className="error_page">
-        <p className="heading1 center mt-5"> Looks Like You have been Lost:(</p>
-        <p className="heading3 center mt-3">
-          Sorry we cant find that page, you will find loads to explore on the home
-          page
-        </p>
-        <Link to="/">
+    <div className="error_page">
+      <p className="heading1 center mt-5"> Looks Like You have been Lost:(</p>
+      <p className="heading3 center mt-3">
+        Sorry we cant find that page, you will find loads to explore on the home
+        page
+      </p>
+      <Link to="/">
         <button className="btn btn-primary mt-4"> Home </button>
       </Link>
-      </div>
+    </div>
   );
 };
 
